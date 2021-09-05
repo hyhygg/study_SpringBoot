f9f9f12dfbeae5046d837326bb964706cfd56da0
hyhygg <yhyhy94@gmail.com>
2021-09-05 16:38:44 +0900

    chap01.3.4.1 웹 애플리케이션 작성_컨트롤러 빈 등록 -- p.50

--- Chapter01/src/main/java/com/rubypaper/Chapter01Application.java
+++ Chapter01/src/main/java/com/rubypaper/Chapter01Application.java
@@ -3,11 +3,13 @@ package com.rubypaper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 스프링 부트로 만든 애플리케이션의 시작 클래스
 @SpringBootApplication
 public class Chapter01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter01Application.class, args);
+	
 	}
-
 }
+
