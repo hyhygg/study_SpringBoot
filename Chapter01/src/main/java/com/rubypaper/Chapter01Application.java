20b2d1f81bd7f9730e8cbd60c894ab42dd3cb978
hyhygg <yhyhy94@gmail.com>
2021-09-06 16:16:48 +0900

    char01.3.4 웹 애플리케이션 작성_REST 컨트롤러, 롬복 라이브러리 -- p.68

--- Chapter01/src/main/java/com/rubypaper/Chapter01Application.java
+++ Chapter01/src/main/java/com/rubypaper/Chapter01Application.java
@@ -2,9 +2,11 @@ package com.rubypaper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 // 스프링 부트로 만든 애플리케이션의 시작 클래스
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.rubypaper", "com.ruby"})
 public class Chapter01Application {
 
 	public static void main(String[] args) {
