6ed783327708d0175499b8f6d1a478d731bb152a
hyhygg <yhyhy94@gmail.com>
2021-09-14 18:30:50 +0900

    chap03.1.1 스프링 부트 테스트 -- p.128

---
+++ Chapter03/src/test/java/com/rubypaper/Chapter03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubypaper;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
