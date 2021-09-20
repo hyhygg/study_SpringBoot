55d4e129aadcadb98170edef862372c014a3a5ad
hyhygg <yhyhy94@gmail.com>
2021-09-20 22:34:28 +0900

    chap03.3 JAR_스프링 부트 빌드, Runnable JAR 실행 -- p.168

--- Chapter03/src/test/java/com/rubypaper/PropertiesTest.java
+++ Chapter03/src/test/java/com/rubypaper/PropertiesTest.java
@@ -7,13 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.rubypaper.controller.BoardController;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=BoardController.class,
-				properties= {"author.name=Gurum",
-							"author.age=45",
-							"author.nation=Korea"})
+@SpringBootTest(properties = {"author.name=Gurum",
+						"author.age=45",
+						"author.nation=Korea"})
 public class PropertiesTest {
 	
 	@Autowired
@@ -21,8 +18,8 @@ public class PropertiesTest {
 	
 	@Test
 	public void testMethod() {
-		System.out.println("이름: " + environment.getProperty("author.name"));
-		System.out.println("나이: " + environment.getProperty("author.age"));
-		System.out.println("국가: " + environment.getProperty("author.nation"));
+		System.out.println("이름: "+environment.getProperty("author.name"));
+		System.out.println("나이: "+environment.getProperty("author.age"));
+		System.out.println("국가: "+environment.getProperty("author.nation"));
 	}
 }
